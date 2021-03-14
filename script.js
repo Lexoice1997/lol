7541af149343fd94f421666cb99a9c660e80cbdb
Lexoice1997 <Lexoice1997@gmail.com>
2021-03-14 12:54:11 +0500

    First practice work.

--- script.js
+++ script.js
@@ -1,119 +1,16 @@
-let calendar = document.querySelector('#calendar');
-let body = calendar.querySelector('.body');
-let prev = calendar.querySelector('.prev');
-let next = calendar.querySelector('.next');
-let info = calendar.querySelector('.info');
+let films = prompt('Сколько фильмов вы уже посмотрели?' , '');
+let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+let rait = prompt('На сколько вы оцените его?', '');
 
-let date  = new Date();
-let year  = date.getFullYear();
-let month = date.getMonth();
-let months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
-draw(body, year, month);
+let personalMovieDB = {
+	count: films,
+	movies: {},
+	actors: {},
+	genres: [],
+	privet: false
+};
 
-next.addEventListener('click', function() {
-	body.innerHTML = '';
-	
-	info.innerHTML = months[month+1] + ' ' + year;
-	if (month == 11) {
-		month = 0;
-		year++;
-		draw(body, year, month);
-	} else {		
-		month++;
-		draw(body, year, month);
-	}
-});
+personalMovieDB.movies[lastFilm] = rait;
 
-prev.addEventListener('click', function() {
-	body.innerHTML = '';
-	draw(body, getPrevYear(year, month), getPrevMonth(month));
-});
-
-function range(count) {
-	let arr = [];
-
-	for (let i = 1; i <= count; i++) {
-		arr.push(i);
-	}
-
-	return arr;
-}
-
-function getLastDay(year, month) {
-	let date = new Date(year, month - 1, 0);
-	let day = date.getDate();
-	return day;
-}
-
-function getFirstWeekDay(year, month) {
-	let date = new Date(year, month);
-	let day = date.getDay();
-	return day;
-}
-
-function getLastWeekDay(year, month) {
-	let date = new Date(year, month, getLastDay(year, month));
-	let day = date.getDay();
-	return day;
-}
-
-function normalize(arr, left, right) {
-	let result = [];
-	for (let i = 0; i < left; i++) {
-		result.push('');
-	}
-	for (let i = 0; i < arr.length; i++) {
-		result.push(arr[i]);
-	}
-	for (let i = 0; i < right; i++) {
-		result.push('');
-	}
-
-	return result;
-}
-
-function chunk(arr, n) {
-	// n - количество элементов в подмассиве
-	let result = [];
-	let k = 0;
-
-	for (let i = 0; i < arr.length / n; i++) {
-		let subarray = [];
-		for (let j = 0; j < n; j++) {
-			subarray.push(arr[k]);
-			k++;
-		}
-
-		result.push(subarray)
-	}
-
-	return result;
-}
-
-function createTable(parent, arr) {
-	for (let i = 0; i < arr.length; i++) {
-		let tr = document.createElement('tr');
-
-		for (let j = 0; j < arr[i].length; j++) {
-			let td = document.createElement('td');
-
-			td.innerHTML = arr[i][j];
-
-			tr.appendChild(td);
-		}
-
-		parent.appendChild(tr);
-		
-	}
-}
-
-function draw(body, year, month) {
-	let arr = range(getLastDay(year, month));
-	
-	let firstWeekDay = getFirstWeekDay(year, month);
-	let lastWeekDay  = getLastWeekDay(year, month);
-
-	let nums = chunk(normalize(arr, firstWeekDay, 6 - lastWeekDay), 7);
-	createTable(body, nums);
-}
+console.log(personalMovieDB)
