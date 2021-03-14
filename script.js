fa5e6a854354f8bcba5f6ed316cb7a5500ea75e8
Lexoice1997 <Lexoice1997@gmail.com>
2021-03-14 20:47:21 +0500

    Second practice work.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 let films = prompt('Сколько фильмов вы уже посмотрели?' , '');
-let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-let rait = prompt('На сколько вы оцените его?', '');
-
 
 let personalMovieDB = {
 	count: films,
@@ -11,6 +8,24 @@ let personalMovieDB = {
 	privet: false
 };
 
-personalMovieDB.movies[lastFilm] = rait;
+for (let i = 0; i < 2; i++) {
+	let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+		rait = prompt('На сколько вы оцените его?', '');
+	if (lastFilm != null && rait != null && lastFilm != '' && rait != '' && lastFilm.length < 50) {
+		personalMovieDB.movies[lastFilm] = rait;
+	} else {
+		i--;
+	}
+}
 
-console.log(personalMovieDB)
+if (personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	alert ('Вы киноман');
+} else {
+	alert ('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
